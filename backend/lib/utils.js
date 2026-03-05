165e5f415712e1635be3fe49e062a698c4819e85
Esubalew <esuwo2024@gmail.com>
2026-03-05 13:56:18 +0300

    changing secure to true for backend

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "none", //use strict for CSRF attacks
-    secure: false, // if u want secure.NODE_ENV === "development" ? true : false,
+    secure: true, // if u want secure.NODE_ENV === "development" ? true : false,
   });
   return token;
 };
