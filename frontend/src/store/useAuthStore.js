14c0476d955fd5ed4188e5c74a49976a96cbd6e4
Esubalew <esuwo2024@gmail.com>
2026-03-09 18:22:32 +0300

    fix: add vercel rewrite for React Router routes

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
-const BASE_URL = "https://chatapp-yg2h.onrender.com" || "/";
+const BASE_URL = "https://chatapp-yg2h.onrender.com";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
